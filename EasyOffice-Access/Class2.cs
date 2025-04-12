ac068ae08ed10d3e109e021d2d2aebadcda86a40
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-04-12 16:37:59 +0300

    creating initial database and creating project and making it in 3 tier structure

---
+++ EasyOffice-Access/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOffice_Access
+{
+    public class Class2
+    {
+        public static string Test1()
+        {
+            return "Test1";
+        }
+    }
+}
---
+++ EasyOffice-Business/Class1.cs
@@ -0,0 +1,17 @@
+﻿using EasyOffice_Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOffice_Business
+{
+    public class Class1
+    {
+        public static string Test1()
+        {
+            return Class2.Test1();
+        }
+    }
+}
